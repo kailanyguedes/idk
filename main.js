145961003dee39874d2d092ecf9b108bb4155d68
Kay ♪ <kailany123hot@gmail.com>
2025-04-15 01:23:59 -0300

    adicionando o main.js e salvando todas as mudanças

---
+++ main.js
@@ -0,0 +1,13 @@
+var typed = new Typed("#typed-text", {
+    strings: [
+      "ADS",
+      "Manutençao",
+      "Desenvolvedora Front-End",
+      "JavaScript"
+    ],
+    typeSpeed: 60,
+    backSpeed: 40,
+    backDelay: 2000,
+    loop: true
+  });
+  
